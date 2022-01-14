8ea4549e386312692af49422928e75fc0aea2b66
EmeeMohamed <80221913+EmeeMohamed@users.noreply.github.com>
2022-01-15 01:56:34 +0300

    Add files via upload

---
+++ simpleCalculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#this is the simple calcularot that takes tow numbers and make the main operations for them(+, -, *, /)
+firstNum = int(input("Please, insert the first number"))
+secondNum = int(input("Please, insert the second number"))
+
+print("The summation of ", firstNum, " + ", secondNum, " = " , firstNum+secondNum, 
+      "\n the subtraction of ", firstNum, " - ", secondNum, " = " , firstNum-secondNum, 
+      "\n the multiplication of ", firstNum, " * ", secondNum, " = " , firstNum*secondNum, 
+      "\n the divition of ", firstNum, " \ ", secondNum, " = " , firstNum/secondNum)
+
+
+# In[ ]:
+
+
+
+
